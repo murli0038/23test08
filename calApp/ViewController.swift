4f73378e0ed1b0b75d345e64455b41cdd242b9d9
COE-029 <coe-029@coe-029s-imac.local>
2019-08-23 16:02:41 +0530

    its my calsi ok bby

--- calApp/ViewController.swift
+++ calApp/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtA: UITextField!
+    
+    @IBOutlet weak var txtB: UITextField!
+    
+    @IBAction func segmentvalue(_ sender: UISegmentedControl)
+    {
+        let a = ((Int)(txtA.text!))!
+        let b = ((Int)(txtB.text!))!
+        let c = 0
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            let c = a + b
+        case 1:
+            let c = a - b
+        case 2:
+            let c = a * b
+        case 3:
+            let c = a / b
+        default:
+            print("Invalid choice")
+        }
+        
+        let alertView = UIAlertController (title: "Answer", message: "Answer is \(c)", preferredStyle:.alert)
+        
+        self.present(alertView,animated: true)
+        {
+            print("After view present")
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
