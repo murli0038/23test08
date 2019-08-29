f566d123736823a82fb8340c83593ee4d7a14ab0
COE-029 <coe-029@coe-029s-imac.local>
2019-08-29 15:40:35 +0530

    its my calsi by 2

--- calApp/ViewController.swift
+++ calApp/ViewController.swift
@@ -16,41 +16,65 @@ class ViewController: UIViewController {
     
     @IBAction func segmentvalue(_ sender: UISegmentedControl)
     {
+        print("the value is \(txtB.text!) & \(txtA.text!)")
+        if (txtB.text) != "" && (txtA.text) != ""
+        {
         let a = ((Int)(txtA.text!))!
         let b = ((Int)(txtB.text!))!
-        let c = 0
+        var c = 0
         
         switch sender.selectedSegmentIndex {
         case 0:
-            let c = a + b
+             c = a + b
         case 1:
-            let c = a - b
+             c = a - b
         case 2:
-            let c = a * b
+             c = a * b
         case 3:
-            let c = a / b
+             c = a / b
         default:
             print("Invalid choice")
         }
         
         let alertView = UIAlertController (title: "Answer", message: "Answer is \(c)", preferredStyle:.alert)
         
+            let btnResign = UIAlertAction(title:"Cancel",style: .cancel)
+            {
+                (action) in self.dismiss(animated: true, completion: nil)
+            }
+            alertView.addAction(btnResign)
+            
+            
         self.present(alertView,animated: true)
         {
             print("After view present")
         }
+            let errorShow = UIAlertController(title: "Error",message:"Please enter value in text box",preferredStyle : .actionSheet)
+            let btnCancle = UIAlertAction(title: "Cancel", style: .cancel)
+            {
+                (action) in
+                self.dismiss(animated: true, completion: nil)
+            }
+            errorShow.addAction(btnCancle)
+            
+            self.present(errorShow,animated: true,completion: nil)
+    }
+            func touchesBegan(_ touches:Set<UITouch>, with event : UIEvent?)
+    {
+        self.view.endEditing(true)
         
     }
-    override func viewDidLoad() {
+        func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
 }
 
